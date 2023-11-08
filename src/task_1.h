f214bf01797be1ffff1a5226803d1ee9073bfa48
Aliyor01 <127713784+Aliyor01@users.noreply.github.com>
2023-11-08 22:58:33 +0500

    Update task_1.h

--- src/task_1.h
+++ src/task_1.h
@@ -1,5 +1,28 @@
 /*
- * Author:
- * Date:
- * Name:
+ * Author: Aliyorjon
+ * Date: 08.11.2023
+ * Name: Task 1
  */
+
+class Node {
+public:
+    int data;
+    Node* next;
+
+    // Constructor
+    Node(int val) : data(val), next(nullptr) {}
+};
+
+class Stack {
+    Node* top;
+public:
+    // Constructor
+    Stack() : top(nullptr) {}
+
+    // Push a value onto the stack
+    void push(int val) {
+        Node* newNode = new Node(val);
+        newNode->next = top;
+        top = newNode;
+    }
+};
